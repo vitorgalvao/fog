5224c7984a6b1f24279807a161a09bc49f936f10
Vítor Galvão <info@vitorgalvao.com>
2018-05-07 23:18:12 +0100

    Remove bind for space key

--- app/webview-script.js
+++ app/webview-script.js
@@ -67,14 +67,6 @@ Mousetrap.bind('command+return', function() {
   if (page_type() === 'episode') document.getElementById('save_episode_button').click();
 });
 
-Mousetrap.bind('space', function(event) {
-  event.preventDefault();
-
-  if (page_type() === 'episode') {
-    document.getElementById('playpausebutton').click();
-  }
-});
-
 // Navigate episode cells
 Mousetrap.bind(['down', 'j'], function() {
   navigate_cells('down');
